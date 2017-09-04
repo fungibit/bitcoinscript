d1a3be0d33bccdd505fad3978029097f700faaed
fungibit <fungibit@yandex.com>
2017-09-04 11:59:41 +0300

    minor refactoring of script-formatting functions

--- bitcoinscript/format.py
+++ bitcoinscript/format.py
@@ -2,10 +2,11 @@
 Tools for formatting scripts as human-readable strings.
 """
 
-from bitcoin.core.script import CScript as _CScript
 from .opcode import get_opcode_by_name, encode_op_pushdata
+from .basic import iter_script_parts
 
 ################################################################################
+# formatting: raw -> formatted string
 
 def format_script(x, delim = ' ', **kwargs):
     """
@@ -16,29 +17,28 @@ def format_script(x, delim = ' ', **kwargs):
     """
     return delim.join(iter_script_parts_as_strings(x, **kwargs))
 
-def iter_script_parts_as_strings(x, max_value_len = 19, empty_value_string = '<<empty>>'):
-    for token in iter_script_parts(x):
-        try:
-            token_str = token.hex()
-        except AttributeError:
-            token_str = str(token)
-        if len(token_str) > max_value_len:
-            part_len = (max_value_len - 3) // 2
-            assert part_len > 0, part_len
-            token_str = '%s...%s' % (token_str[:part_len], token_str[-part_len:])
-        if len(token_str) == 0:
-            token_str = empty_value_string
-        yield token_str
+def iter_script_parts_as_strings(x, **kwargs):
+    return (
+        format_data_token(token, **kwargs)
+        for token in iter_script_parts(x)
+    )
+
+def format_data_token(token, max_value_len = 19, empty_value_string = '<<empty>>'):
+    try:
+        token_str = token.hex()
+    except AttributeError:
+        token_str = str(token)
+    if len(token_str) > max_value_len and not token_str.startswith('OP_'):
+        part_len = (max_value_len - 3) // 2
+        assert part_len > 0, part_len
+        token_str = '%s...%s' % (token_str[:part_len], token_str[-part_len:])
+    if len(token_str) == 0:
+        token_str = empty_value_string
+    return token_str
+
+################################################################################
+# parsing: formatted string -> raw
     
-def iter_script_parts(x):
-    x = getattr(x, 'raw', x)
-    x = _CScript(x)
-    return iter(x)
-
-def raw_iter_script(x):
-    x = getattr(x, 'raw', x)
-    return list(_CScript(x).raw_iter())
-
 def parse_formatted_script(x):
     """
     The opposite of `format_script`.
